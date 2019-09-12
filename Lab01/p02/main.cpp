78ed7e4d32016dcddcddd6d2d22855d9ec6a072d
Isa Dzhumabaev <isa.dzhumabaev@zensoft.io>
2019-09-12 20:40:27 +0600

    Lab02

--- Lab01/p02/main.cpp
+++ Lab01/p02/main.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
-#include <iomanip>
-
+#include <limits>
+#include <cstdlib>
 using namespace std;
 
 int main() {
-	cout << "sec: ";
-	int sec;
-	cin >> sec;
+	cout << "size of int: " << sizeof(int) << "\n";
+	cout << "max int: " << numeric_limits<int>::max() << "\n";
+	cout << "min int: " << numeric_limits<int>::min() << "\n";
+
+	int x, y;
+	cout << "1st number: ";
+	cin >> x;
+	cout << "2nd number: ";
+	cin >> y;
+
+	cout << "distande between " << x << " and " << y << " is " << abs(x - y) << "\n";
 
 
-	int hour = sec / 3600 % 24;
+	// int sum, diff, prod, qtn, rem;
+	// sum = x + y;
+	// diff = x - y;
+	// prod = x * y;
+	// qtn = x / y;
+	// rem = x % y;
 
-	sec %= 3600;
-
-	int min = sec / 60;
-	sec %= 60;
-
-	cout << setfill('0')
-		 << setw(2) << hour << ":"
-		 << setw(2) << min << ":"
-		 << setw(2) << sec << "\n";	
+	// cout << x << " + " << y << " = " << sum << "\n";
+	// cout << x << " - " << y << " = " << diff << "\n";
+	// cout << x << " * " << y << " = " << prod << "\n";
+	// cout << x << " / " << y << " = " << qtn << "\n";
+	// cout << x << " % " << y << " = " << rem << "\n";
 
 }
